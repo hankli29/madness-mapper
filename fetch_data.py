da7d4369e635cefb1bf876107a158fc27853cdd8
hankli29 <hankli3000@gmail.com>
2026-03-23 20:33:33 -0400

    Create function to make API call and return json formatted data

--- fetch_data.py
+++ fetch_data.py
@@ -9,13 +9,20 @@ load_dotenv() # makes everything in the .env file visible to program
 
 # access .env variables (api keys)
 odds_key = os.getenv("ODDS_API_KEY")
-kaggle_key = os.getenv("KAGGLE_API_TOKEN")
 
-# get() method returns a response object (a package containing data, headers, status code)
-# url must be entered as a string
-response = requests.get(f"https://api.the-odds-api.com/v4/sports/basketball_ncaab/odds?apiKey={odds_key}&regions=us&markets=h2h&oddsFormat=american")
-print(f"Requests remaining: {response.headers['x-requests-remaining']}")
-print(f"API call status code: {response.status_code}\n")
+# put logic of making API call and fetching data all in a method
+# allows importing module to control when to make calls, otherwise a call would be made for every single import
+def get_data():
+    # get() method returns a response object (a package containing data, headers, status code)
+    # url must be entered as a string
+    response = requests.get(f"https://api.the-odds-api.com/v4/sports/basketball_ncaab/odds?apiKey={odds_key}&regions=us&markets=h2h&oddsFormat=american")
+    print(f"Requests remaining: {response.headers['x-requests-remaining']}")
+    print(f"API call status code: {response.status_code}\n")
 
-odds_data = response.json()
-print(odds_data)
+    odds_data = response.json()
+
+    return odds_data
+
+if __name__ == "__main__":
+    test_data = get_data()
+    print(test_data)
