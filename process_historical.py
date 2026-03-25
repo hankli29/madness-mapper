68c2bffb63b44dec37194f3ea48c321034757fc0
hankli29 <hankli3000@gmail.com>
2026-03-25 19:17:08 -0400

    Extracted individual team stats and game outcomes for model training

--- process_historical.py
+++ process_historical.py
@@ -7,20 +7,22 @@ path = kagglehub.dataset_download("nishaanamin/march-madness-data")
 print("Path to dataset files:", path)
 
 kenpom_bart_df = pd.read_csv(f"{path}/KenPom Barttorvik.csv")
-# print(kenpom_bart_df.head()) # prints the first 5 rows of data from the dataframe
+# print(kenpom_bart_df.head()) # prints the first 5 rows of data from the  
 
-# indexing into a dataframe returns a sub-table/sub-dataframe
-# single key/column for a series, list of keys for a dataframe
+# indexing into a data frame returns a sub-table/sub-data frame
+# single key/column for a series, list of keys for a data frame
 # only want to focus on these select data columns
-kenpom_bart_df = kenpom_bart_df[["YEAR", "TEAM", "SEED", "ROUND", "KADJ O", "KADJ D", "KADJ EM", "BADJ EM", "BADJ O", "BADJ D", "BARTHAG", "WIN%", "EXP", "TALENT", "ELITE SOS"]]
+kenpom_bart_df = kenpom_bart_df[["YEAR", "TEAM", "SEED", "ROUND", "KADJ O", "KADJ D", "KADJ EM", "BADJ O", "BADJ D", "BADJ EM", "BARTHAG", "WIN%", "EXP", "TALENT", "ELITE SOS"]]
 # data for current year (2026) has every team ROUND stat = 0, this will skew data
 
 # kenpom_bart_df["ROUND"] > 0 creates a boolean mask (true for rows where condition is true, false otherwise)
-# kenpom_bart_df[<boolean mask>] returns dataframe only containing "true" rows
+# kenpom_bart_df[<boolean mask>] returns data frame only containing "true" rows
 kenpom_bart_df = kenpom_bart_df[kenpom_bart_df["ROUND"] > 0]
 # after removing 2026 data, row index will no longer start from 0
-# must reset index, use drop arg to get rid of old index col
-kenpom_bart_df = kenpom_bart_df.reset_index(drop=True)
+# must reset index, use drop arg to get rid of old index col, inplace to modify df in place
+kenpom_bart_df.reset_index(drop=True, inplace=True)
+# set multi-index to (YEAR, TEAM) -> can retrieve rows according to specific YEAR and TEAM values
+kenpom_bart_df.set_index(["YEAR", "TEAM"], inplace=True)
 
 # print(kenpom_bart_df.head())
 
@@ -28,28 +30,45 @@ kenpom_bart_df = kenpom_bart_df.reset_index(drop=True)
 # YEAR and TEAM to match outcome with corresponding team's stats, score to determine winner
 matchups_df = pd.read_csv(f"{path}/Tournament Matchups.csv")[["YEAR", "TEAM", "SCORE"]]
 matchups_df = matchups_df[matchups_df["YEAR"] < 2026].reset_index(drop=True)
-print(matchups_df.head())
+# print(matchups_df.head())
 
 i = 0
+# create empty df with specified columns
+match_stats_df = pd.DataFrame()
+data_rows = []
 while (i < len(matchups_df)): # len(df) returns num ROWS
     winner = None
 
+    # indexing with single braces returns series, indices are column names
     row1 = matchups_df.iloc[i]
-    team1 = row1["TEAM"] # access values in row series using col name
+    team1 = row1["TEAM"].strip() # access values in row series using col name, also remove whitespace as it can cause lookup failure
     score1 = row1["SCORE"]
 
     row2 = matchups_df.iloc[i + 1]
-    team2 = row2["TEAM"]
+    team2 = row2["TEAM"].strip()
     score2 = row2["SCORE"]
 
     year = row1["YEAR"]
 
+    # the winner should be stored as a number: the model learns from numbers, so team name as text wouldn't be helpful
     if score1 > score2:
-        winner = team1
+        winner = 1
     else:
-        winner = team2
+        winner = 2
     
-    
-    
+    # indexing with double braces returns a data frame, NOT a series
+    t1_data = kenpom_bart_df.loc[[(year, team1)]]
+    t2_data = kenpom_bart_df.loc[[(year, team2)]]
 
+    # rename the columns of each individual data frame to avoid name collisions -> prevent ambiguity
+    # multi-indices were originally different -> reset index to have resulting data frame be single row and avoid NaNs
+    data_row = pd.concat([t1_data.add_suffix(" T1").reset_index(drop=True), t2_data.add_suffix(" T2").reset_index(drop=True)], axis = 1)
+    data_row["WINNER"] = winner # create new column with winner (crete using key, value pair)
+
+    data_rows.append(data_row)
+    
     i += 2
+
+# concatenating vertically keeps each row's row index (all 0s in this case) -> must reset index
+match_stats_df = pd.concat(data_rows).reset_index(drop=True)
+# print(match_stats_df.head())
