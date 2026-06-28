ba1e37b93f633a328d65a8aabfa51289d58fd630
Hank Li <hankli3000@gmail.com>
2026-06-28 16:20:33 -0500

    add guards for failed api call, concat odds data to stats for prediction

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,6 +1,8 @@
 import pickle
 import pandas as pd
 from pathlib import Path
+from process_live import find_game, process_odds
+from fetch_data import get_data
 
 base_dir = Path(__file__).resolve().parent.parent
 
@@ -13,7 +15,19 @@ def predict_winner(team1, team2):
     t1_data = current_data[current_data["TEAM"] == team1]
     t2_data = current_data[current_data["TEAM"] == team2]
 
-    prediction_data = pd.concat([t1_data.add_suffix(" T1").reset_index(drop=True), t2_data.add_suffix(" T2").reset_index(drop=True)], axis = 1).drop(["TEAM T1", "TEAM T2"], axis=1)
+    try:
+        odds_data = get_data()
+        game_data = find_game(odds_data, team1, team2)
+    except Exception as e:
+        game_data = None
+
+    prediction_odds = pd.DataFrame([process_odds(game_data, team1, team2)])
+
+    prediction_stats = pd.concat([t1_data.add_suffix(" T1").reset_index(drop=True), t2_data.add_suffix(" T2").reset_index(drop=True)], axis = 1).drop(["TEAM T1", "TEAM T2"], axis=1)
+    prediction_data = pd.concat([prediction_stats, prediction_odds], axis=1)
+
+    print(prediction_data.head())
+
     probabilities = model.predict_proba(prediction_data)[0]
     # return winner and the confidence level
     if probabilities[0] > probabilities[1]:
