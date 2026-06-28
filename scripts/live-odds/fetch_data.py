c9e9285b69477d1a57d0c515f97a55e4655291dc
Hank Li <hankli3000@gmail.com>
2026-06-28 14:50:44 -0500

    add spreads and totals as market params for api call, remove unnecessary documentation

--- scripts/live-odds/fetch_data.py
+++ scripts/live-odds/fetch_data.py
@@ -1,23 +1,17 @@
-# use pip install to install public/external libraries on global machine
-
-# must then actually import those libraries into this file to use them
 import requests
 from dotenv import load_dotenv
-import os # os lib is used to access environment variables (API keys)
+import os # os lib is used to access environment variables
 
 load_dotenv() # makes everything in the .env file visible to program
-
-# access .env variables (api keys)
 odds_key = os.getenv("ODDS_API_KEY")
 
-# put logic of making API call and fetching data all in a method
-# allows importing module to control when to make calls, otherwise a call would be made for every single import
 def get_data():
-    # get() method returns a response object (a package containing data, headers, status code)
-    # url must be entered as a string
-    response = requests.get(f"https://api.the-odds-api.com/v4/sports/basketball_ncaab/odds?apiKey={odds_key}&regions=us&markets=h2h&oddsFormat=american")
-    print(f"Requests remaining: {response.headers['x-requests-remaining']}")
-    print(f"API call status code: {response.status_code}\n")
+
+    # get() method returns a response object (containing data, headers, status code)
+    response = requests.get(f"https://api.the-odds-api.com/v4/sports/basketball_ncaab/odds?apiKey={odds_key}&regions=us&markets=h2h,spreads,totals&oddsFormat=american")
+    response.raise_for_status()
+    # print(f"Requests remaining: {response.headers['x-requests-remaining']}")
+    # print(f"API call status code: {response.status_code}\n")
 
     odds_data = response.json()
 
